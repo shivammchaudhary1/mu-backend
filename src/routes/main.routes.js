ece49a07de86c1bbaf2d1ba54e98a91f5a660a85
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2025-11-14 12:53:37 +0530

    leads routing

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,7 +1,9 @@
 import authRoutes from "./auth.routes.js";
+import leadRoutes from "./lead.routes.js";
 
 const mainRoutes = (app) => {
   app.use("/api/auth", authRoutes);
+  app.use("/api/leads", leadRoutes);
 };
 
 export default mainRoutes;
